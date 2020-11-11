019daa259a0d19d3cbbc83719e5c2ba7aeec7935
Nathan Li <nathanli1995@gmail.com>
2020-11-10 23:25:56 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pdf-reader
+
+this pdf reader first requires another program to convert a PDF file to text first
+https://github.com/pdfminer/pdfminer.six
+
+then reader.py reads the output. you can keep track of where you are by taking note of the sentence number that prints in the console. 
+
+# to-do
+figure out how to keep track of it by pdf page numbers
---
+++ util/reader.py
@@ -0,0 +1,32 @@
+import pyttsx3
+import os
+import re
+
+
+os.chdir("C:\\Users\\nathan\\Dropbox\\Books\\Grimgar")
+
+engine = pyttsx3.init()
+
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[1].id)
+
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 125)
+
+count = 0
+start = 412
+# pdf page = 25
+with open("output.txt", 'rb') as fp:
+    strList = fp.readlines()
+    decodedList = [ line.decode("utf-8").strip() for line in strList ]
+    bigString = " ".join(decodedList)
+    sentenceSplit = re.split("\. |\? |\! ", bigString)
+
+    for line in sentenceSplit:
+        count+=1
+        if count >= start:
+            engine.say(line.strip())
+            engine.runAndWait()
+            print(count)
+
+
